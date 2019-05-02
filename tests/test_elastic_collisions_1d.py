b28b23eedad9f6bf95d903b3bd56d70792c1c252
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2019-05-02 11:31:27 -0400

    Changes made during tutorial

--- tests/test_elastic_collisions_1d.py
+++ tests/test_elastic_collisions_1d.py
@@ -1,13 +1,20 @@
 import pytest
 
 from pycontest import elastic_collisions as ec
-from pycontest.utils import momentum, E_kin
 
 
 # a simple tests for equal masses (balls should exchange velocities)
 
 # using default values of m1 and m2
 def test_collision_1d_1():
-    v1_f, v2_f = ec.collision_1d(v1_i=1, v2_i=-2)
+    v1_i = 1
+    v2_i = -2
+    v1_f, v2_f = ec.collision_1d(v1_i=v1_i, v2_i=v2_i)
+    assert(v1_i == v2_f and v2_i == v1_f)
 
 
+def test_collision_1d_2():
+    v1_i = 1
+    v2_i = -2
+    v1_f, v2_f = ec.collision_1d(v1_i=v1_i, v2_i=v2_i, m1=1, m2=10**7)
+    assert pytest.approx(v1_i == v2_f and v2_i == v1_f)
--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -5,8 +5,8 @@ import numpy as np
 import pytest
 
 
-
-def test_simulation_1():
+@pytest.mark.parametrize("dt", [0.1, 0.1])
+def test_simulation_1(dt):
     # initial condition and simulation parameters
     domain = ([-2, 12], [0, 3])
     t_max = 6
@@ -29,8 +29,24 @@ def test_simulation_1():
     # Y component should stay the same
     assert (loc[0][1], loc[1][1]) == (loc_0[0][1], loc_0[1][1])
 
+
     # particles will exchange vel_x
     assert vel[0][0] == -1
     assert vel[1][0] == 1
     # Y component should stay the same
     assert (vel[0][1], vel[1][1]) == (vel_0[0][1], vel_0[1][1])
+
+
+def test_simulation_2():
+    # initial condition and simulation parameters
+    domain = ([-2, 12], [0, 3])
+    t_max = 6
+    dt = 0.5
+    loc_0 = np.array([[0, 1.5],[10, 1.5]])
+    vel_0 = np.array([[1, 0], [-1, 0]])
+    radius = 1
+    mass = [1, 1]
+
+    with pytest.raises(Exception):
+        # running the simulation
+        loc, vel = sim.simulation(t_max, dt, mass, radius, loc_0, vel_0, domain)
